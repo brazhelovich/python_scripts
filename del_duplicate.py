6be0a43129c95bbe88b5cd632bb0f28ef86dce96
Andrei Brazhalovich <andrei_brazhalovich@epam.com>
2020-10-19 19:20:09 +0300

    added additional comments

--- del_duplicate.py
+++ del_duplicate.py
@@ -1,6 +1,10 @@
-#this script deletes dupliate line in text file
+#script deletes dupliate line in text file
 
 file ='emails.txt'
 
 uniqlines = set(open(file, 'r', encoding='utf-8').readlines())
 gotovo = open('email_output.txt', 'w', encoding='utf-8').writelines(set(uniqlines))
+
+
+#just test
+#it is working
